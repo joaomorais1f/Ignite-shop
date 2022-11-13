cf6f2b2b888b4828be3c1a80f02c806ec8f06d52
joaomorais1f <ifsp.joaov@gmail.com>
2022-11-13 18:57:04 -0300

    adding header compoent

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,17 @@
 import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/globa'
+import LogoImg from '../assets/Logo.svg'
+import { Container, Header } from '../styles/screen/app'
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={LogoImg.src} />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
