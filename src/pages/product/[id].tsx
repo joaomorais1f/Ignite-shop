0370f61735d30f1a3a329f562f76d7206c4ee810
joaomorais1f <ifsp.joaov@gmail.com>
2022-11-15 17:49:44 -0300

    adding payment function

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -2,8 +2,11 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import Stripe from "stripe";
+import axios from "axios";
+
 import { stripe } from "../../lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/screen/product";
+import { useState } from "react";
 
 interface ProductProps {
   product: {
@@ -12,11 +15,31 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   }
 }
 
 export default function Product({ product }: ProductProps) {
   const { isFallback } = useRouter()
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true)
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data
+
+      window.location.href = checkoutUrl
+    } catch (err) {
+      // Datadog / Sentry
+      setIsCreatingCheckoutSession(false);
+      alert("Falha ao redirecionar ao checkout")
+    }
+  }
 
   if (isFallback) {
     return <h1> Loading </h1>
@@ -33,7 +56,7 @@ export default function Product({ product }: ProductProps) {
 
         <p> {product.description} </p>
 
-        <button>
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
           Comprar Agora
         </button>
       </ProductDetails>
@@ -70,6 +93,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
           currency: 'BRL',
         }).format(Number(price.unit_amount) / 100),
         description: product.description,
+        defaultPriceId: price.id
       }
     },
     revalidate: 60 * 60 * 1 // 1 hour
