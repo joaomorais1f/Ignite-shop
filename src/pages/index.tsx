7aafbad5d55caf6a144f88f9e4fdfd2bcfe78d77
joaomorais1f <ifsp.joaov@gmail.com>
2022-11-15 15:26:19 -0300

    adding stripe api and list products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
+import { GetServerSideProps } from "next";
 import Image from "next/image";
+import { stripe } from "../lib/stripe";
+
 import { useKeenSlider } from "keen-slider/react";
 
 import { HomeContainer, Product } from "../styles/screen/home";
 
-import shirt1 from "../assets/shirts/1.png";
-import shirt2 from "../assets/shirts/2.png";
-import shirt3 from "../assets/shirts/3.png";
+import shirt1 from "../assets/shirts/1.png"
 
 import 'keen-slider/keen-slider.min.css';
+import Stripe from "stripe";
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -19,38 +30,42 @@ export default function Home() {
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      <Product className="keen-slider__slide">
-        <Image src={shirt1} width={520} height={480} alt="shirt" />
-
-        <footer>
-          <strong> Camiseta Y </strong>
-          <span> R$ 79,90 </span>  
-        </footer>
-      </Product>
-      <Product className="keen-slider__slide">
-        <Image src={shirt2} width={520} height={480} alt="shirt" />
-
-        <footer>
-          <strong> Camiseta W </strong>
-          <span> R$ 79,90 </span>  
-        </footer>
-      </Product>
-      <Product className="keen-slider__slide">
-        <Image src={shirt3} width={520} height={480} alt="shirt" />
-
-        <footer>
-          <strong> Camiseta Z </strong>
-          <span> R$ 79,90 </span>  
-        </footer>
-      </Product>
-      <Product className="keen-slider__slide">
-        <Image src={shirt3} width={520} height={480} alt="shirt" />
-
-        <footer>
-          <strong> Camiseta Z </strong>
-          <span> R$ 79,90 </span>  
-        </footer>
-      </Product>
+      {products.map(product => {
+        return (
+        <Product key={product.id} className="keen-slider__slide">
+          <Image src={product.imageUrl} width={520} height={480} alt="shirt" />
+  
+          <footer>
+            <strong> {product.name} </strong>
+            <span> {product.price} </span>  
+          </footer>
+        </Product>
+        )
+      })}
     </HomeContainer>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount / 100,
+    }
+  })
+
+  return {
+    props: {
+      products
+    }
+  }
 }
